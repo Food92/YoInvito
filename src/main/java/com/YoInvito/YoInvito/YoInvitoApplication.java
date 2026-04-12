641c68c6a2030c2bcba245532365472c3dc2be5e
foodf <fo.fontus@duocuc.cl>
2026-04-12 02:42:06 -0400

    Mi primer commit

---
+++ src/main/java/com/YoInvito/YoInvito/YoInvitoApplication.java
@@ -0,0 +1,13 @@
+package com.YoInvito.YoInvito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoInvitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoInvitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YoInvito/YoInvito/YoInvitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YoInvito.YoInvito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoInvitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
